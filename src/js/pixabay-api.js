cb866f2dbac70bc954b8b49312671d058eee119e
AnnaOsadchuk <119502034+AnnaOsadchuk@users.noreply.github.com>
2024-03-08 21:11:54 +0100

    changed to axios

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,4 +1,6 @@
-export default function searchPixa(searchQuery, page = 1) {
+import axios from 'axios';
+
+export default async function searchPixa(searchQuery, page = 1) {
   const BASE_URL = 'https://pixabay.com/api/';
   const API_KEY = '36683727-e929c153efd1b0d2cc7c4f38c';
   const QUERY = `${searchQuery}`;
@@ -7,25 +9,18 @@ export default function searchPixa(searchQuery, page = 1) {
   const SEFESEARCH = 'true';
   const PAGE = `${page}`;
   const PER_PAGE = 15;
-  const Link = `${BASE_URL}?key=${API_KEY}&q=${QUERY}&image_type=${IMAGE_TYPE}&orientastion=${ORIENTATION}&seferearch=${SEFESEARCH}&page=${PAGE}&per_page=${PER_PAGE}`;
+  const link = `${BASE_URL}?key=${API_KEY}&q=${QUERY}&image_type=${IMAGE_TYPE}&orientastion=${ORIENTATION}&seferearch=${SEFESEARCH}&page=${PAGE}&per_page=${PER_PAGE}`;
 
-  const message =
-    'Sorry, there are no images matching your search query. Please try again!';
+  try {
+    const { data } = await axios.get(link);
+    const isNextPage = PAGE * PER_PAGE <= data.totalHits ? true : false;
 
-  return fetch(Link)
-    .then(resp => {
-      if (!resp.ok) {
-        throw new Error(message);
-      }
-      return resp.json();
-    })
-    .then(data => {
-      if (data.hits.length === 0) {
-        throw new Error(message);
-      }
+    if (data.hits.length === 0) {
+      throw 'Sorry, there are no images matching your search query. Please try again!';
+    }
 
-      const isNextPage = PAGE * PER_PAGE <= data.totalHits ? true : false;
-
-      return [data.hits, isNextPage];
-    });
+    return [data.hits, isNextPage];
+  } catch (error) {
+    throw new Error(error);
+  }
 }
